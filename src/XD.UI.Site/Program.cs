77a363b521f0546a21fe5e6fef68efe354dec136
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-01-17 18:36:04 -0400

    AppModelo: Passo 3 - Pastas MVC Adicionadas, HomeController criada e adicionada, Views/Home/Index configurada

--- src/XD.UI.Site/Program.cs
+++ src/XD.UI.Site/Program.cs
@@ -1,21 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using XD.UI.Site;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+startup.Configure(app, app.Environment);
 app.Run();
